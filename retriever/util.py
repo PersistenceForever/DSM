e3046e9b2b042babc1305dd8f6f2e3ca9e83920a
PersistenceForever <guoshashaxing@163.com>
2022-06-27 19:35:59 +0800

    Update util.py

--- retriever/util.py
+++ retriever/util.py
@@ -7,12 +7,27 @@ def load_data(datasets):
     adj = adj.astype('int')
     nb_nodes = np.max(adj) + 1
     edge_index = adj.T
-    print('Load the edge_index done!')    
-   
+    print('Load the edge_index done!')  
+
     # load initial features
     feats = np.load('./features/'+datasets+'_feature1024.npy')
     return edge_index, feats, nb_nodes
 
+
+def load_RGCNdata(datasets):
+    # load the adjacency
+    data = np.loadtxt('./RGCN_Data/'+datasets+'.txt')
+    adj = data[:, 0:2]
+    r = data[:, -1]
+    num_rels = len(set(r))
+    adj = adj.astype('int')
+    nb_nodes = np.max(adj) + 1
+    edge_index = adj.T
+    print('Load the edge_index done!')
+    
+    # load initial features
+    feats = np.load('./features/'+datasets+'_feature1024.npy')
+    return edge_index, r, num_rels, feats, nb_nodes
 
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, inputs):
